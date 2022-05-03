bbbf679411ad901c4dd395fc4809778c538ad0c3
thiagsv <86622333+thiagsv@users.noreply.github.com>
2022-05-03 11:30:32 -0300

    Correções no código

--- funcoes.h
+++ funcoes.h
@@ -5,8 +5,8 @@
 int imprimeMenu() {
     int opcao = 0; 
 
-    printf("1 - Acessar as opções de dicionário");
-    scanf("%d", opcao);
+    printf("1 - Acessar as opções de dicionário: ");
+    scanf("%d", &opcao);
 
     return opcao;
 }
@@ -16,12 +16,12 @@ void opcoesDicionario(FILE *dicionario) {
     char caracteres[100000000];
 
 
-    printf("1 - Ver dicionário\n2 - Inserir nova palavra\n3 - Voltar ao menu anterior");
-    scanf("%d", opcaoDicionario);
+    printf("1 - Ver dicionário\n2 - Inserir nova palavra\n3 - Voltar ao menu anterior: ");
+    scanf("%d", &opcaoDicionario);
 
     while(opcaoDicionario != 1 || opcaoDicionario != 2 ||opcaoDicionario != 3) { //prevenção de erros
         printf("Por favor, digite um número válido!: ");
-        scanf("%d", opcaoDicionario);
+        scanf("%d", &opcaoDicionario);
     }
 
     while(opcaoDicionario != 3) { //enquanto não tentar retornar ao menu continua o mesmo processo
@@ -37,21 +37,21 @@ void opcoesDicionario(FILE *dicionario) {
             char significado[10000];
 
             printf("Digite a palavra desejada (caso queira sair digite 0): ");
-            scanf("%s", palavra);
+            scanf("%s", &palavra);
 
             while (palavra[0] != '0') {
                 printf("Digite o siginificado da palavra resumida (máx: 10000 carcteres)): ");
-                scanf("%s", significado);
+                scanf("%s", &significado);
                 fputs(palavra, dicionario);
                 fputs(" - ", dicionario);
                 fputs(significado, dicionario);
                 printf("Digite a palavra desejada (caso queira sair digite 0): ");
-                scanf("%s", palavra);
+                scanf("%s", &palavra);
             }
         }
 
-        printf("1 - Ver dicionário\n2 - Inserir nova palavra\n3 - Voltar ao menu anterior");
-        scanf("%d", opcaoDicionario);
+        printf("1 - Ver dicionário\n2 - Inserir nova palavra\n3 - Voltar ao menu anterior: ");
+        scanf("%d", &opcaoDicionario);
     }
 
     if(opcaoDicionario == 3) {
@@ -61,10 +61,8 @@ void opcoesDicionario(FILE *dicionario) {
 
 int quantidadeCaracteresArquivo(FILE *dicionario) { //retorna quantidade de caracteres do dicionario
     int count = 1;
-    int flag = 0;
 
     while(!feof(dicionario)) {
-		c = getc(dicionario);
         count++;
 	}
 
